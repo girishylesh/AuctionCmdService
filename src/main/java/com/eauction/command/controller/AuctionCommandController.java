cac9c3e92e81069dd9b5a72deee27680d5e67432
shylesh <shyleshgiri.tv@cognizant.com>
2022-07-03 11:56:40 +0530

    Enabled cross origin

--- src/main/java/com/eauction/command/controller/AuctionCommandController.java
+++ src/main/java/com/eauction/command/controller/AuctionCommandController.java
@@ -10,6 +10,7 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/e-auction/api/v1/cmd")
 public class AuctionCommandController {
 
